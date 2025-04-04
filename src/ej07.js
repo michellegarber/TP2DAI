9d2d8977d146ca2f3bb6559c81b63346ed462178
MICHELLE SOFIA GARBER <48521612@ort>
2025-04-04 08:38:19 -0300

    actualizacion 7

--- src/ej07.js
+++ src/ej07.js
@@ -1,24 +1,3 @@
-/*import { getCurrencyAbbreviation, getCurrency } from 'currency-map-country';
-let monedaDelPais, codigoPais;
-codigoPais = 'AR';
-monedaDelPais = obtenerMoneda(codigoPais);
-console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
-
-codigoPais = 'UZA';
-monedaDelPais = obtenerMoneda(codigoPais);
-console.log(`La moneda del país ${codigoPais} es: ${monedaDelPais}`);
-
-function obtenerMoneda(codigoPais) {
-    const abreviaturaPais = getCurrencyAbbreviation(codigoPais);
-    console.log(`Abreviatura para ${codigoPais}:`, abreviaturaPais); 
-    if (!abreviaturaPais) {
-        return null;
-    }
-    const nombre = getCurrency(abreviaturaPais);
-    return nombre ? nombre.name : null;
-}
-*/
-// Importamos las funciones de la librería
 import { getCurrencyAbbreviation, getCurrency } from 'currency-map-country';
 
 let monedaDelPais, codigoPais;
